ba00841039fd937fcc5d768f4e83721f3e1fde25
zunaldo <zunaldojepara123@gmail.com>
2023-10-06 08:25:41 +0700

    create Register View

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class RegisterController extends Controller
 {
     public function index(){
-        return 'kiu kiuu';
+        return view ('Register');
     }
 }
---
+++ resources/views/Register.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="input-group mb-3">
+        <span class="input-group-text" id="basic-addon1"></span>
+        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+      </div>
+
+      <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="basic-addon2">
+        <span class="input-group-text" id="basic-addon2"></span>
+      </div>
+      
+      <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="basic-addon3">
+        <span class="input-group-text" id="basic-addon3"></span>
+      </div>
+      
+      <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="basic-addon4">
+        <span class="input-group-text" id="basic-addon4"></span>
+      </div>
+      
+      
+</body>
+</html>
